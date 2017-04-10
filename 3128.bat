835263cd2b89958d805f3578228ee744e2a48c6b
Charlie <chenlei.hu@mail.utoronto.ca>
2017-04-10 16:23:55 -0400

    add batch file to use monitor program in BA3128

---
+++ 3128.bat
@@ -0,0 +1,4 @@
+set GCC_EXEC_PREFIX=
+C:
+cd %QUARTUS_ROOTDIR%\..\University_Program\Monitor_Program
+start %QUARTUS_ROOTDIR%\bin64\jre64\bin\javaw.exe -Dsun.java2d.noddraw=true -jar %QUARTUS_ROOTDIR%\..\University_Program\Monitor_Program\bin\Altera_Monitor_Program.jar
